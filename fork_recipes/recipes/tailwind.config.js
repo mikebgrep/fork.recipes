5cddde9132b41631eb84131c9a50d5bc7935a32a
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2024-12-08 15:43:38 +0200

    remove debug flag from settings, setup tailwindcss locally not from cdn

--- fork_recipes/recipes/tailwind.config.js
+++ fork_recipes/recipes/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-                    './templates/**/*.html'
-          ],
-          darkMode: 'class',
+    './templates/**/*.html'
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
-    fontFamily: {
+      fontFamily: {
         body: [
           'Inter',
           'ui-sans-serif',
@@ -43,33 +43,34 @@ module.exports = {
         ],
       },
 
-                        colors: {
-                        mint: {
-                            50: '#edfbf6',
-                            100: '#d3f5e8',
-                            200: '#56cc9d',
-                            300: '#56cc9d',
-                            400: '#56cc9d',
-                            500: '#56cc9d',
-                            600: '#4db88d',
-                            700: '#44a47e',
-                            800: '#3b906e',
-                            900: '#327c5f',
-                        },
-                        primary: {
-                50: '#edfbf6',
-                100: '#d3f5e8',
-                200: '#56cc9d',
-                300: '#56cc9d',
-                400: '#56cc9d',
-                500: '#56cc9d',
-                600: '#4db88d',
-                700: '#44a47e',
-                800: '#3b906e',
-                900: '#327c5f',
-            },
+      colors: {
+        mint: {
+          50: '#edfbf6',
+          100: '#d3f5e8',
+          200: '#56cc9d',
+          300: '#56cc9d',
+          400: '#56cc9d',
+          500: '#56cc9d',
+          600: '#4db88d',
+          700: '#44a47e',
+          800: '#3b906e',
+          900: '#327c5f',
+        },
+        primary: {
+          50: '#edfbf6',
+          100: '#d3f5e8',
+          200: '#56cc9d',
+          300: '#56cc9d',
+          400: '#56cc9d',
+          500: '#56cc9d',
+          600: '#4db88d',
+          700: '#44a47e',
+          800: '#3b906e',
+          900: '#327c5f',
+        },
 
-                    }},
+      }
+    },
   },
   plugins: [],
 };
