12503e587dc788258eae1eafe6dc29fd57f3a825
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2025-01-04 16:36:45 +0200

    generate css, add source in recipe details, delete not needed base.html

--- fork_recipes/recipes/tailwind.config.js
+++ fork_recipes/recipes/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './templates/**/*.html'
-  ],
+content: ['./templates/**/*.html'],
   darkMode: 'class',
   theme: {
     extend: {
