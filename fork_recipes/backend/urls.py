1b16face432eb5a5eec7d82fd2b4e7950c1e5c7c
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2025-01-04 13:58:35 +0200

    adding static url for debug

--- fork_recipes/backend/urls.py
+++ fork_recipes/backend/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from recipes import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 if settings.DEBUG:
     urlpatterns = [
         path('', include('recipes.urls', namespace="recipes")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
 else:
     urlpatterns = [
         path('', include('recipes.urls', namespace="recipes")),
